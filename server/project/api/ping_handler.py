d770a50fd0d09b9053615185055b3cfb51f6d15d
Kenneth <kcheung331@gmail.com>
2020-02-10 17:05:43 -0800

    ping endpoint converted

--- server/project/api/ping_handler.py
+++ server/project/api/ping_handler.py
@@ -1,16 +1,20 @@
 import json
-from flask import jsonify, request, Blueprint
 import os
+
+from flask import Blueprint, request
+from flask_restx import Resource
+from project.api import api
+
 ping_handler = Blueprint('ping_handler', __name__)
 
 
-@ping_handler.route('/ping', methods=['POST'])
-def ping():
-    TEAM_NAME = os.environ['TEAM_NAME']
-    if request.method == 'POST':
-        body = json.loads(request.get_data())
-        print(TEAM_NAME.split(','))
-        if body['teamName'] in TEAM_NAME.split(','):
-            return jsonify({'response': "{} is now part of the team".format(body['teamName'])}), 200
-        else:
-            return jsonify({'response': "{} is not part of the team, change your .env".format(body['teamName'])}), 400
+@api.route('/ping')
+class Ping(Resource):
+    def post(self):
+        TEAM_NAME = os.environ['TEAM_NAME']
+        if request.method == 'POST':
+            body = json.loads(request.get_data())
+            if body['teamName'] in TEAM_NAME.split(','):
+                return {'response': "{} is now part of the team".format(body['teamName'])}, 200
+            else:
+                return {'response': "{} is not part of the team, change your .env".format(body['teamName'])}, 400
