b9398b2d549cf99bd5503546cf27942ecff822eb
Gerardo Paredes <gerardparedes23@gmail.com>
2020-02-13 11:03:15 -0800

    progress bar animation added

--- client/src/pages/ProfileSetupPage/ProfileStepsHeader/ProfileStepsHeader.js
+++ client/src/pages/ProfileSetupPage/ProfileStepsHeader/ProfileStepsHeader.js
@@ -3,15 +3,19 @@ import { Box, Typography } from '@material-ui/core';
 
 const ProfileStepsHeader = ({step, text}) => {
     let width;
+    let barStyles;
     switch(step) {
         case 1:
             width = '32%';
+            barStyles = 'stepHeader__bar--inner32';
             break;
         case 2:
             width = '64%'
+            barStyles = 'stepHeader__bar--inner64';
             break;
         case 3:
             width = '100%';
+            barStyles = 'stepHeader__bar--inner100';
             break;
         default:
             width = '32%';
@@ -21,8 +25,8 @@ const ProfileStepsHeader = ({step, text}) => {
             <Typography className="stepHeader__title" variant="h5">{text}</Typography>
             <div className="stepHeader__bar">
                 <div
-                    style={{ width: width }}
-                    className="stepHeader__bar--inner"></div>
+                    className={barStyles}
+                    style={{ width: width }}></div>
             </div>
         </Box>
     )
@@ -30,3 +34,4 @@ const ProfileStepsHeader = ({step, text}) => {
 };
 
 export default ProfileStepsHeader;
+
