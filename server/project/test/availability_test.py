21a4c3c878d7fd626c2dcd2f3a91562b8880fc60
Brian Haley <brian_haley@live.ca>
2020-02-18 14:53:06 -0500

    Availability FK test

--- server/project/test/availability_test.py
+++ server/project/test/availability_test.py
@@ -30,3 +30,19 @@ class AvailabilityModelTest(TestBase):
 
         self.assertEqual(availability.sunday, sunday)
         self.assertEqual(availability.end, end)
+
+    def test_availability_foreign_key(self):
+        """Tests whether the foreign key relationship is working between
+        Availability and Event."""
+        url = 'acleverurl'
+        add_event(url=url)
+        event_id = Event.query.filter_by(url=url).first().id
+        start = dt.time(6)
+        add_availability(start=start, event_id=event_id)
+        db.session.commit()
+        availability, event = db.session.query(Availability, Event)\
+            .join()\
+            .first()
+
+        self.assertEqual(availability.start, start)
+        self.assertEqual(event.url, url)
