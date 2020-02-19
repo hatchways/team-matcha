c0f9596c0343bb06147151547e2fafdd1a016326
Brian Haley <brian_haley@live.ca>
2020-02-18 23:19:30 -0500

    colour -> color

--- server/project/models/event.py
+++ server/project/models/event.py
@@ -10,7 +10,7 @@ class Event(db.Model):
     description = db.Column(db.String(1024))
     duration = db.Column(db.Integer, nullable=False)
     url = db.Column(db.String(32), nullable=False, unique=True)
-    colour = db.Column(db.String(6), nullable=False)
+    color = db.Column(db.String(6), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     availability_id = db.Column(db.Integer, db.ForeignKey('availability.id'),
                                 nullable=False)
@@ -21,10 +21,10 @@ class Event(db.Model):
 
 def add_event(user_id, availability, name='my event', location='my home',
               description='A cool event', duration=60, url='mycoolevent',
-              colour='FFC0CB'):
+              color='FFC0CB'):
     """Add's a row to the event table as well as the availability table."""
     event = Event(name=name, location=location, description=description,
-                  duration=duration, url=url, colour=colour, user_id=user_id,
+                  duration=duration, url=url, color=color, user_id=user_id,
                   availability=availability)
     db.session.add(event)
     db.session.add(availability)
