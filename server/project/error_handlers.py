24a2279d03ec53891d57057e19561f922cd36ba8
Kenneth <kcheung331@gmail.com>
2020-02-20 11:17:08 -0800

    Remove unused Error

--- server/project/error_handlers.py
+++ server/project/error_handlers.py
@@ -10,11 +10,7 @@ class Error(Exception):
    pass
 
 class BlacklistTokenError(Error):
-   """Raised when Token has been blacklisted"""
-   pass
-
-class AccessDeniedError(Error):
-   """Raised when  has been blacklisted"""
+   """Custom Error when Token has been blacklisted"""
    pass
 
 @api.errorhandler(PermissionError)
