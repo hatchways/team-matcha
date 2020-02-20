89ca8c2866560f51c52a852905697f299a99ed18
Brian Haley <brian_haley@live.ca>
2020-02-19 20:08:46 -0500

    Working GET, POST

--- server/project/api/events_handler.py
+++ server/project/api/events_handler.py
@@ -1,16 +1,22 @@
 from flask import Blueprint, abort
 from flask_restx import Resource, fields
 from project import db, api
-from project.api.models import Availability, Event, User
+from project.models.availability import Availability, create_availability
+from project.models.user import User, add_user
+from project.models.event import Event, add_event
+from typing import Tuple
+from project.decorators import token_required
+from project.error_handlers import *
+import datetime as dt
 
 events_blueprint = Blueprint('events', __name__)
 
 weekday = fields.Boolean(
     default=True, example=True,
-    description='Whether an event should be scheduled on this day.')
+    description='Whether an event should be scheduled on this day')
 weekend = fields.Boolean(
     default=False, example=False,
-    description='Whether an event should be scheduled on this day.')
+    description='Whether an event should be scheduled on this day')
 days_input = api.model(
     'Days', {
         'sunday': weekend,
@@ -23,54 +29,68 @@ days_input = api.model(
     })
 availability_input = api.model(
     'Availability', {
-        'days': fields.Nested(days_input),
         'start': fields.Integer(
             description='Your earliest availability for the event',
-            required=True, example=8),
+            default=8, example=8),
         'end': fields.Integer(
-            description='Your latest availability for the event.',
-            required=True, example=17)})
-event_input = api.model(
+            description='Your latest availability for the event',
+            default=17, example=17),
+        'days': fields.Nested(days_input)})
+event_input_output = api.model(
     'Event', {
         'name': fields.String(
-            description='The name of the event.', required=True,
-            example='Party!', max_length=32),
+            description='The name of the event', required=True,
+            example='My event', max_length=32),
         'location': fields.String(
+            default='',
             description='The location where the event will take place',
-            example='101 Example St.', max_length=256),
+            example='My office', max_length=256),
         'description': fields.String(
-            description='A description for the event.',
+            default='', description='A description for the event',
             example='This is an awesome description.', max_length=1024),
+        'duration': fields.Integer(
+            description='The duration of the event in minutes', default=60,
+            example=60),
         'url': fields.String(
-            description='The unique url for this event.', required=True,
+            description='The unique url for this event', required=True,
             example='myevent', max_length=32),
-        'colour': fields.String(
-            description='A hex representation of a colour without the #.',
-            required=True, example='FFC0CB', max_length=6),
+        'color': fields.String(
+            description="A hex representation of a colour with the leading '#'",
+            required=True, example='#000000', max_length=7),
         'availability': fields.Nested(availability_input)})
 
 
-def add_event(params, user):
-    pass
-    if params['name']:
-        name = params['name']
-    name = params['name']
-    user_id = User.query.filter_by(public_id=user).first().id
-    availability = Availability()  # TODO
-    event = Event(name, location, description, url, colour, user_id,
-                  availability_id)
-    db.session.add(availability)
-    db.session.add(event)
-    db.session.commit()
-    return event
-
-
 @api.route('/users/<user>/events')
-class Event(Resource):
+class Events(Resource):
+    @api.marshal_with(event_input_output, envelope='data')
     def get(self, user):
-        return {'message': 'success'}, 200
+        """Returns all the events that the user has created."""
+        events = Event.query.join(User).filter(User.public_id == user).all()
+        return events, 200
 
-    @api.expect(event_input, validate=True)
+    @api.expect(event_input_output, validate=True)
     def post(self, user):
-        event = add_event(api.payload, user)
-        return {'message': 'success'}, 201
+        """Creates an Event for the specified user."""
+        payload = api.payload
+        user_id = User.query.filter_by(public_id=user).first().id
+        availability = create_availability(
+            sunday=payload['availability']['days']['sunday'],
+            monday=payload['availability']['days']['monday'],
+            tuesday=payload['availability']['days']['tuesday'],
+            wednesday=payload['availability']['days']['wednesday'],
+            thursday=payload['availability']['days']['thursday'],
+            friday=payload['availability']['days']['friday'],
+            saturday=payload['availability']['days']['saturday'],
+            start=dt.time(payload['availability']['start']),
+            end=dt.time(payload['availability']['end']))
+        add_event(
+            user_id=user_id,
+            availability=availability,
+            name=payload['name'],
+            location=payload['location'],
+            description=payload['description'],
+            duration=payload['duration'],
+            url=payload['url'],
+            color=payload['color'].lstrip('#'))
+        db.session.commit()
+        return 'success', 201
--- server/project/test/event_handler_test.py
+++ server/project/test/event_handler_test.py
@@ -1,26 +1,68 @@
 import json
 from project import db
-from project.api.models import Availability, Event, User
 from project.test.test_base import TestBase
-from project.test.availability_test import add_availability
-from project.test.event_test import add_event
-from project.test.user_test import add_user
+from project.models.availability import Availability, create_availability
+from project.models.event import Event, add_event
+from project.models.user import User, add_user
+
+
+def create_event_json(name='My event', location='', description='', duration=60,
+                      url='myevent', color='#000000', start=8, end=17,
+                      sunday=False, monday=True, tuesday=True, wednesday=True,
+                      thursday=True, friday=True, saturday=False,):
+    """
+    Creates JSON dump to create an Event.
+    :param name: name of the event
+    :param location: location of the event
+    :param description: description of the event
+    :param duration: duration of the event in minutes
+    :param url: url suffix of the event
+    :param color: color of the event including the preceeding '#'
+    :param start: start of the event in hours
+    :param end: end of the event in hours
+    :param sunday: whether the event can scheduled on sunday
+    :param monday: whether the event can scheduled on monday
+    :param tuesday: whether the event can scheduled on tuesday
+    :param wednesday: whether the event can scheduled on wednesday
+    :param thursday: whether the event can scheduled on thursday
+    :param friday: whether the event can scheduled on friday
+    :param saturday: whether the event can scheduled on saturday
+    :return: JSON dump
+    """
+    data = {
+        'name': name,
+        'location': location,
+        'description': description,
+        'duration': duration,
+        'url': url,
+        'color': color,
+        'availability': {
+            'start': start,
+            'end': end,
+            'days': {
+                'sunday': sunday,
+                'monday': monday,
+                'tuesday': tuesday,
+                'wednesday': wednesday,
+                'thursday': thursday,
+                'friday': friday,
+                'saturday': saturday
+            }
+        }
+    }
+    return json.dumps(data)
 
 
 class EventCreateTest(TestBase):
     def test_add_event(self):
-        user = add_user()
-        availability = {'days': {'sunday': False, 'monday': True,
-                                 'tuesday': True, 'wednesday': True,
-                                 'thursday': True, 'friday': True,
-                                 'saturday': False},
-                        'start': 8, 'end': 17}
-        event = {'name': 'My first event', 'location': 'My home',
-                 'description': 'A completely legit event.',
-                 'url': 'myfirstevent', 'colour': '0000ff',
-                 'availability': availability}
-        data = json.dumps(event)
-        response = self.api.post(f'/users/{user.public_id}/events', data=data,
+        add_user()
+        db.session.commit()
+        url = 'clickme'
+        data = create_event_json(url=url)
+        user_public_id = User.query.first().public_id
+        response = self.api.post(f'/users/{user_public_id}/events', data=data,
                                  content_type='application/json')
+        event = Event.query.filter_by(url=url).first()
 
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(event.url, url)
