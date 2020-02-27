39b2daca437fc62f324d9f0fa1d03209f7ef02d0
Kenneth <kcheung331@gmail.com>
2020-02-27 14:17:39 -0800

    remove commented code, fix timezone import api

--- server/project/models/creds.py
+++ server/project/models/creds.py
@@ -7,11 +7,7 @@ class Credential(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     access_token = db.Column(db.String(2048), unique=True, nullable=False)
     refresh_token = db.Column(db.String(2048), unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-    # def __init__(self, api_key=access_token, refresh_token=None):
-    #     self.api_key = api_key
-    #     self.refresh_token = refresh_token
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=False )
 
 
 def add_cred(access_token="MY_API_KEY", refresh_token=None):
