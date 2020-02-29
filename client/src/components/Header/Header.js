f0a9a245701276ce3d299d6c335e9a006a74866b
Gerardo Paredes <gerardparedes23@gmail.com>
2020-02-28 16:50:51 -0800

    mobile-nav & spinner added to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,10 +3,18 @@ import React, { useContext, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Box, Typography } from '@material-ui/core';
 import RouteContext from '../../Context/Context';
+// importing components
+import Spinner from '../Spinners/SpinnerSmall';
+import MobileNav from '../MobileNav/MobileNav';
 
 const Header = ({ isActive }) => {
     const { token, isAuth, handleLogout } = useContext(RouteContext);
-    const [user, setUser] = useState({ userDetails: {} });
+    const [user, setUser] = useState({ userDetails: { img_url: '' } });
+
+    const [active, setToggle] = useState(false);
+    const toggle = () => {
+        setToggle(!active);
+    };
 
     useEffect(() => {
         fetch(`/users/details`, {
@@ -48,29 +56,44 @@ const Header = ({ isActive }) => {
                 </NavLink>
                 <NavLink 
                     activeClassName="header__nav--link--isActive"
-                    className="header__nav--link " 
+                    className="header__nav--link header__nav--link--upgrade" 
                     to="/upgrade"
                 >Upgrade Account
                 </NavLink>
                 <Box className="header__nav--profile">
-                    <img 
-                        className="header__nav--profile--img"
-                        src={user.img_url} 
-                        alt=""
-                    />
-                    <Typography 
-                        className="header__nav--profile--name"
-                        variant="body1">
-                        {user.name}
-                    </Typography>
+                    {
+                        user.img_url !== undefined
+                        ?  ( 
+                            <React.Fragment>
+                                <img 
+                                    className="header__nav--profile--img"
+                                    src={user.img_url.length > 0 ? user.img_url : 'https://i.ya-webdesign.com/images/default-avatar-png-18.png' } 
+                                    alt=""/> 
+                                <Typography 
+                                    className="header__nav--profile--name"
+                                    variant="body1">
+                                    {user.name}
+                                </Typography>
+                            </React.Fragment>)
+                            : <Spinner />
+                    }
+                    
                 </Box>
                 {
                     isAuth ? <button className="header__nav--logout" onClick={handleLogout}>Logout</button> : null
                 }
             </nav>
+            <MobileNav 
+                isActive={isActive}
+                user={user}
+                toggle={toggle}
+                active={active}
+                handleLogout={handleLogout}
+            />
         </Box>
     </Box>
     )
 };
 
 export default Header;
+
