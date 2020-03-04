15391e5458978f60e8d1701237b609b0b4c918e7
Kenneth <kcheung331@gmail.com>
2020-03-03 16:07:19 -0800

    user roles added, promote function, promote on event creation

--- server/project/models/user.py
+++ server/project/models/user.py
@@ -5,11 +5,20 @@ import jwt
 from flask import current_app
 from project import db
 from project.error_handlers import BlacklistTokenError
+from sqlalchemy import Enum
 from project.models.blacklist_token import BlacklistToken
+import enum
 
 #-------------------------------------------------------------------------------
 # Model
 #-------------------------------------------------------------------------------
+
+roles = ('ON_BOARDING', 'MEMBER', 'ADMIN')
+
+class Role(enum.Enum):
+    ON_BOARDING = 1
+    MEMBER = 2
+    ADMIN = 3
 
 
 class User(db.Model):
@@ -21,11 +30,12 @@ class User(db.Model):
     name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     events = db.relationship('Event', backref='user')
+    role = db.Column(Enum(Role))
     img_url = db.Column(db.String(2048), unique=True)
     cred = db.relationship('Credential',
-                            backref='user',
-                            cascade="all, delete-orphan",
-                            uselist=False)
+                           backref='user',
+                           cascade="all, delete-orphan",
+                           uselist=False)
 
     @staticmethod
     def decode_auth_token(auth_token):
@@ -79,14 +89,15 @@ class User(db.Model):
 #-------------------------------------------------------------------------------
 
 
-def add_user(name='kenny', email='test@email.com', **kwargs) -> User:
+def add_user(name='kenny', email='test@email.com',
+             role=Role.ON_BOARDING, **kwargs) -> User:
     """
     Creates a User, adds the created user and returns the created User.
     :param name: (str) the name of the user
     :param email: (str) the unique email of the user
     :return: a User
     """
-    user = User(public_id=uuid.uuid4(), name=name, email=email)
+    user = User(public_id=uuid.uuid4(), name=name, email=email, role=role)
     db.session.add(user)
     return user
 
@@ -95,5 +106,10 @@ def update_user(user, params):
     for key, value in params.items():
         if value is not None:
             setattr(user, key, value)
+    db.session.commit()
+    return user
+
+def promote_to_member(user):
+    user.role = Role.MEMBER
     db.session.commit()
     return user
