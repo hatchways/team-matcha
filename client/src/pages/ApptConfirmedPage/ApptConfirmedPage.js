fc7e4aa0e3728c2d65f99f0846d11ab12bc30205
Gerardo Paredes <gerardparedes23@gmail.com>
2020-03-05 14:05:09 -0800

    add loader to confirmation page

--- client/src/pages/ApptConfirmedPage/ApptConfirmedPage.js
+++ client/src/pages/ApptConfirmedPage/ApptConfirmedPage.js
@@ -1,78 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import momentTZ from "moment-timezone";
 // import moment from 'moment';
 import { Box, Typography } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import ScheduleIcon from '@material-ui/icons/Schedule';
+import LoadingPage from '../../components/LoadingPage/LoadingPage';
 // import PublicIcon from '@material-ui/icons/Public';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
-class ApptConfirmedPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            event: {
-                owner: '',
-                name: '',
-                duration: '',
-                location: '',
-                color: ''
-            }
-        }
-    }
+const ApptConfirmedPage = (props) => {
+    const [event, setEvent] = useState({});
+    const [apptStart, setApptStart] = useState('');
+    const [apptEnd, setApptEnd] = useState('');
+    const [loading, setIsLoading] = useState(true);
 
-    componentDidMount(){
-        this.handleFetchEvent();
-        // this.handleFetchApptDetails();
-    }
 
-    handleFetchEvent = () => {
-        const { public_id, eventLink, appointmentId } = this.props.match.params; // get params from url
+    useEffect(() => {
+        const { public_id, eventLink, appointmentId } = props.match.params; // get params from url
         fetch(`/users/${public_id}/events/${eventLink}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-            })
-            .then(data => data.json())
-            .then((eventData) => {
-                console.log(eventData);
-                this.setState({
-                    event: {
-                        ...this.state.event, 
-                            color: eventData.color,
-                            owner: public_id,
-                            name: eventData.name,
-                            duration: eventData.duration,
-                            location: eventData.location.length > 0 ? eventData.location : 'N/A'
-                    },
-                    apptStart: momentTZ(`${appointmentId}`).utcOffset(appointmentId).format('hh:mma'),
-                    apptEnd: momentTZ(`${appointmentId}`).utcOffset(appointmentId).add(eventData.duration,'m').format('hh:mma'),
-                })
-            })
-            .catch(err => (err));
-    }
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+        })
+        .then(data => data.json())
+        .then((eventData) => {
+            setEvent({...eventData});
+            setApptStart(momentTZ(`${appointmentId}`).utcOffset(appointmentId).format('hh:mma'));
+            setApptEnd(momentTZ(`${appointmentId}`).utcOffset(appointmentId).add(eventData.duration,'m').format('hh:mma'));
+            setIsLoading(false);
+        })
+        .catch(err => (err));
+    }, []);
 
-    // handleFetchApptDetails = () => {
-    //     const { public_id, eventLink, appointmentId } = this.props.match.params; // get params from url
-    //     fetch(`/users/${public_id}/events/${eventLink}/appointments/${appointmentId}`, {
-    //         method: 'GET',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         }
-    //         })
-    //         .then(data => data.json())
-    //         .then((apptData) => {
-    //             console.log('appointment data', apptData);
-    //         })
-    //         .catch(err => (err));
-    // }
 
-    render() {
-        const { public_id, appointmentId } = this.props.match.params;
+        const { public_id, appointmentId } = props.match.params;
         const userEventScheduleFor = momentTZ(`${appointmentId}`).utcOffset(appointmentId).format('dddd, MMMM Do, YYYY Z'); // formats invitee meeting (local-time)
 
         return (
+            <React.Fragment>
+            { loading ? <LoadingPage /> : 
             <Box className="apptConfirmed">
                 <Box boxShadow={3} className="apptConfirmed__container">
                     <div className="ribbon ribbon-top-right"><span>Powered By<br/>CalendApp</span></div>
@@ -82,14 +49,14 @@ class ApptConfirmedPage extends React.Component{
                     </Box>
                     <Box>
                         <Box className="apptConfirmed__content">
-                            <FiberManualRecordIcon className="apptConfirmed__content--icon" style={{ color: `${this.state.event.color}` }} />
+                            <FiberManualRecordIcon className="apptConfirmed__content--icon" style={{ color: `${event.color}` }} />
                             &nbsp;
-                            <Typography className="apptConfirmed__content--duration" variant="body1">{this.state.event.duration}min</Typography>
+                            <Typography className="apptConfirmed__content--duration" variant="body1">{event.duration}min</Typography>
                         </Box>
                         <Box className="apptConfirmed__content">
                             <ScheduleIcon className="apptConfirmed__content--icon" style={{ color: "#ef6c00" }}/>
                             &nbsp;
-                            <Typography className="apptConfirmed__content--date" variant="body1">{this.state.apptStart} - {this.state.apptEnd}, {userEventScheduleFor}</Typography>
+                            <Typography className="apptConfirmed__content--date" variant="body1">{apptStart} - {apptEnd}, {userEventScheduleFor}</Typography>
                         </Box>
                         {/*
                         <Box className="apptConfirmed__content">
@@ -101,7 +68,7 @@ class ApptConfirmedPage extends React.Component{
                         <Box className="apptConfirmed__content">
                             <LocationOnIcon className="apptConfirmed__content--icon apptConfirmed__content--icon--light" />
                             &nbsp;
-                            <Typography className="apptConfirmed__content--location" variant="body1">{this.state.event.location}</Typography>
+                            <Typography className="apptConfirmed__content--location" variant="body1">{event.location}</Typography>
                         </Box>
                     </Box>
                     <Box className="apptConfirmed__footer">
@@ -109,8 +76,9 @@ class ApptConfirmedPage extends React.Component{
                     </Box>
                 </Box>
             </Box>
+            }
+            </React.Fragment>
         )
     }
-}
 
 export default ApptConfirmedPage;
