1865d5f186c70a2352f44bbc4cb6292b3cc53b22
Gerardo Paredes <gerardparedes23@gmail.com>
2020-03-05 10:13:50 -0800

    fix timeslot rendering

--- client/src/pages/CalendarPage/TimeSlotItem/TimeSlotItem.js
+++ client/src/pages/CalendarPage/TimeSlotItem/TimeSlotItem.js
@@ -7,11 +7,13 @@ const TimeSlotItem = ({ hour, minute, handleConfirmModal, timezoneName, daySelec
     const formattedTimeSlot24hr = momentTZ(`${hourFormatted}:${minute}`, 'HH:mm').format('HH:mm:ss'); // formatting time to reflect hour&minutes in 24hr format
     const timezoneOffset = momentTZ.tz(timezoneName).format('Z'); // gets the utc timezone offset based on timezone selected
     const dateSelected = `${daySelected}T${formattedTimeSlot24hr}`; // formats the date selected from the calendar
-    const dateToSubmit = `${daySelected}T${formattedTimeSlot24hr}${timezoneOffset}`; // formats the date for appointment submission
-    const dynamicTimeSlot = momentTZ(dateSelected).tz(timezoneName).format('h:mm a'); // formats the date for dynamic slot rendering
+
+    // const dateToSubmit = `${daySelected}T${formattedTimeSlot24hr}${timezoneOffset}`; // formats the date for appointment submission
+    // # working solution
+    const dynamicTimeSlot = momentTZ(dateSelected).format('h:mm a'); // formats the date for dynamic slot rendering
     return (
         <React.Fragment>
-            <Button onClick={() => handleConfirmModal(dynamicTimeSlot, formattedTimeSlot24hr, dateToSubmit)} className="calendarPage__timeslots--btn">{dynamicTimeSlot}</Button>
+            <Button onClick={() => handleConfirmModal(dynamicTimeSlot, formattedTimeSlot24hr, (dateSelected+timezoneOffset))} className="calendarPage__timeslots--btn">{dynamicTimeSlot}</Button>
         </React.Fragment>
     )
 }
