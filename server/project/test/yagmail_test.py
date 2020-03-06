ecfa761056b560565038978609f4a1323ba5439d
Brian Haley <brian_haley@live.ca>
2020-03-05 19:09:57 -0500

    Working test

--- server/project/test/yagmail_test.py
+++ server/project/test/yagmail_test.py
@@ -2,7 +2,9 @@ from __future__ import print_function
 from unittest import TestCase
 import os
 import yagmail as yag
-
+import codecs
+from string import Template
+import datetime as dt
 
 ###########
 # Warning #
@@ -32,3 +34,30 @@ class YagmailTest(TestCase):
                                     preview_only=True)
         self.assertEqual(len(test_email[0]), 1)
         self.assertEqual(test_email[0][0], recipient)
+
+
+    def test_template_generation(self):
+        """Tests whether an email can be successfully generated from an email
+        template."""
+        template = Template(codecs.open(
+            'project/resources/templates/email_template.html',
+            'r',
+            'utf-8'
+        ).read())
+        start = dt.datetime.now(dt.timezone.utc)
+        end = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=1)
+        content = template.substitute(event_name='A cool event',
+                                      event_description='You should come it '
+                                                        'will be a blast!',
+                                      event_location='Everywhere!',
+                                      appointment_start=start.strftime(
+                                          '%A, %B %-d, %Y, %I:%M %p'),
+                                      appointment_end=end.strftime(
+                                          '%A, %B %-d, %Y, %I:%M %p'),
+                                      participant_name='Little Johnny',
+                                      appointment_comments="I haven't seen you "
+                                                           "in forever I am so "
+                                                           "looking forward to "
+                                                           "it!",)
+
+        self.assertTrue('A cool event' in content)
